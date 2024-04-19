6f1273c5c93ac417ad75578abf32546076f8ed18
Sveta28U <152779991+Sveta28U@users.noreply.github.com>
2024-04-19 22:05:30 +0300

    commit add

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,6 @@
 function isEnoughCapacity(products, containerSize) {
   let totalProducts = 0;
-  for (let key in products);
-  {
+  for (let key in products) {
     totalProducts += products[key];
   }
   return totalProducts <= containerSize;
