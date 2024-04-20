74046c6dff74b899bd8f06c94be7171c8c7c9583
Sveta28U <152779991+Sveta28U@users.noreply.github.com>
2024-04-20 10:07:37 +0300

    correction  cod 4

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,6 @@
 function calcAverageCalories(days) {
   let totalCalories = 0;
-  for (day of deys) {
+  for (const day of days) {
     totalCalories += day.calories;
   }
   return totalCalories / days.length;
