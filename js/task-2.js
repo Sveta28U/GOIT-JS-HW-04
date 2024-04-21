de9e3beebc9a5fc1faff282b3d701cb0e690fdbc
Sveta28U <152779991+Sveta28U@users.noreply.github.com>
2024-04-21 12:34:51 +0300

    commit add 5

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,10 @@
 function calcAverageCalories(days) {
+  if (days.length === 0) return 0;
   let totalCalories = 0;
   for (const day of days) {
     totalCalories += day.calories;
   }
+
   return totalCalories / days.length;
 }
 
